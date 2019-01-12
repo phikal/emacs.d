aaf7aabbf0970dbae85b6dfeb7af760086e19ecc
Philip K <philip@warpmail.net>
2019-01-12 23:22:18 +0100

    Require subr-x before initializing anything

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
 ;; (package-initialize) - prevent package.el from adding stuff
 (require 'cl-lib)
+(require 'subr-x)
 (let ((file-name-handler-alist nil)
       (gc-cons-threshold (lsh 1 30))
       (conf-org (expand-file-name "conf.org" user-emacs-directory))
