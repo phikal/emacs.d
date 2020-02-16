aab1bbaedacd05ef4227b72608dd3f5a52cca06c
Philip K <philip@warpmail.net>
2020-02-17 00:53:00 +0100

    Increased gc-cons-threshold to most-positive-fixnum during startup

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 	  (frame-inhibit-implied-resize t)
       (package-enable-at-startup nil)
       (message-log-max 16384)
-      (gc-cons-threshold (lsh 1 30))
+      (gc-cons-threshold most-positive-fixnum)
       (gc-cons-percentage 0.6)
       (auto-window-vscroll nil))
   (org-babel-load-file (expand-file-name "conf.org" user-emacs-directory))
